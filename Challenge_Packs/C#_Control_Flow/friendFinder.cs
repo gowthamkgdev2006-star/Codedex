896052ed7998decd151ef66890777a5f19778241
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-02 01:01:40 +0530

    Add friendFinder program for matching criteria

---
+++ Challenge_Packs/C#_Control_Flow/friendFinder.cs
@@ -0,0 +1,18 @@
+using System;
+
+class friendFinder{
+  static void Main(){
+  int age = 22;
+  bool likesPizza = true;
+  int distance = 10;
+
+  if(age >= 20 && likesPizza == true && distance <= 5)
+  {
+    Console.WriteLine("Swipe right! 👉");
+  }
+  else
+  {
+    Console.WriteLine("Swipe left. 👈");
+  }
+}
+}
